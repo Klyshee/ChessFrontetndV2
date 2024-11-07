a8a737c7349b065f099b9f8e44693bb493614e36
Klyshee <kzujkov86@gmail.com>
2024-11-07 12:35:14 +0300

    Удаление Backend части

--- chess-backend/models/game.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const gameSchema = new mongoose.Schema({
-  fen: { type: String, required: true },
-  history: { type: [String], default: [] }
-});
-
-module.exports = mongoose.model('Game', gameSchema);
